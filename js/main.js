ab10375e7d37ba52061c780b135ae881d94c69db
Wojciech Wichary <brylocode@gmail.com>
2021-04-21 00:53:41 +0200

    fix instruction

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector('.nav');
 
 window.addEventListener('scroll', () => {
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     // console.log(window.innerWidth);
 
     window.scrollY > 0 ? nav.classList.add('nav--sticky') : nav.classList.remove('nav--sticky');
@@ -10,10 +10,15 @@ window.addEventListener('scroll', () => {
 
 const instruction = document.querySelector('.instruction');
 const instructionIcon = document.querySelector('.instruction-icon');
+const instructionCloseBtn = document.querySelector('.instruction__close-btn-js');
 
 
 const handleShowInstruction = () => {
-    instruction.classList.toggle('instruction--active')
+    instruction.classList.add('instruction--active')
+}
+const handleCloseInstruction = () => {
+    instruction.classList.remove('instruction--active')
 }
 
 instructionIcon.addEventListener('click', handleShowInstruction)
+instructionCloseBtn.addEventListener('click', handleCloseInstruction)
